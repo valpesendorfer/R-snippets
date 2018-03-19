d1cb7de8439597d52de066510ebf32d8726934cd
Valentin Pesendorfer <valentin.pesendorfer@boku.ac.at>
2018-03-19 17:13:28 +0100

    add splitfun

---
+++ splitfun.R
@@ -0,0 +1,52 @@
+splitfun <- function(x,n){
+
+## Function randomly splitting data into groups
+##
+## Usage:
+##
+## x ... dataframe or matrix
+## n ... number of groups
+##
+## returns: group column will be appended to input
+
+
+  # empty group vector
+  grp <- rep(0,nrow(x))
+
+  # handle uneven number of observations per group, shuffle final distribution to avoid last group bias
+  obs <- rep(floor(nrow(x)/n),(n-1))
+
+  obs <- sample(c(obs,nrow(x)-sum(obs)))
+
+  # row vector for observation selection
+  rws <- 1:nrow(x)
+
+  # first group
+  ix <- sample(rws,obs[1],replace = F)
+
+  grp[ix] <- 1
+
+  # exclusion vector
+  exc <- ix
+
+  for (ii in 2:(n-1)){
+
+
+    ix <- sample(rws[-exc],obs[ii],replace = F)
+
+    grp[ix] <- ii
+
+    exc <- c(exc,ix)
+
+  }
+
+  # last group
+  ix <- rws[-exec]
+
+  grp[ix] <- n
+
+  # append to input
+  x <- cbind(x,grp)
+
+  return(x)
+}
